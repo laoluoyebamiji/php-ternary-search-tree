c8dcbfeb5a03684d97e08c593c8856d9128d1a10
owen@borseth.us <owen@borseth.us@f5661935-2e4c-0410-b50d-ab345adafc93>
2008-05-01 17:10:17 +0000

    Initial checkin.

---
+++ trunk/classes/TernaryTree.php
@@ -0,0 +1,125 @@
+<?
+
+class TernaryTree
+{
+    private $tree;
+
+    function insert(&$s)
+    {
+	$this->insertWord($this->tree, str_split($s));
+    }
+
+    function search(&$s, $t = "i")
+    {
+	if($t == "i")
+	    return($this->searchWordIterative(str_split($s)));
+	else
+	    return($this->searchWordRecursive($this->tree, str_split($s)));
+    }
+
+    function traverse()
+    {
+	$this->traverseTree($this->tree);
+    }
+
+    function searchNear(&$s, $d)
+    {
+	$this->searchNearPrivate($this->tree, str_split($s), $d);
+    }
+
+    private function searchNearPrivate(&$p, &$s, $d)
+    {
+	// stub for future near string searches
+    }
+
+    private function traverseTree(&$p)
+    {
+	// broken
+
+	if(!$p) return;
+
+	$this->traverseTree($p->low);
+	
+	if($p->data)
+	    $this->traverseTree($p->equal);
+	else
+	    echo($p->equal."\n");
+
+	$this->traverseTree($p->high);
+    }
+
+    private function searchWordRecursive(&$p, &$s)
+    {
+	if(!$p) return(0);
+
+	if(current($s) < $p->data)
+	{
+	    return($this->searchWordRecursive($p->low, $s));
+	}
+	elseif(current($s) > $p->data)
+	{
+	    return($this->searchWordRecursive($p->high, $s));
+	}
+	else
+	{
+	    if(current($s) == FALSE) return(1);
+	    next($s);
+	    return($this->searchWordRecursive($p->equal, $s));
+	}
+    }
+
+    private function searchWordIterative(&$s)
+    {
+	$p = &$this->tree;
+
+	while($p)
+   	{
+	    if(current($s) < $p->data)
+	    {
+		$p = &$p->low;
+	    }
+	    elseif(current($s) == $p->data)
+	    {
+		if(current($s) == FALSE) return(1);
+		next($s);
+		$p = &$p->equal;
+	    }
+	    else
+	    {
+		$p = &$p->high;
+	    }
+	}
+
+	return(0);
+    }
+
+    private function insertWord(&$p, &$s)
+    {
+	if(!$p)
+	{
+	    $p = new TernaryNode();
+	    $p->data = ((current($s)) ? current($s) : NULL);
+	}
+
+	if(current($s) < $p->data)
+	{
+	    $p->low = $this->insertWord($p->low, $s);
+	}
+	elseif(current($s) == $p->data)
+	{
+	    if(current($s))
+	    {
+		next($s);
+		$p->equal = $this->insertWord($p->equal, $s);
+	    }
+	}
+	else
+	{
+	    $p->high = $this->insertWord($p->high, $s);
+	}
+
+	return($p);
+    }
+}
+
+?>
---
+++ trunk/example.php
@@ -0,0 +1,45 @@
+<?
+
+ini_set("memory_limit", "1G");
+ini_set("max_execution_time", "0");
+
+$searchWord = $argv[1];
+
+require("./classes/TernaryNode.php");
+require("./classes/TernaryTree.php");
+
+//$wordArray = array("in");
+$wordArray = array("in", "be", "of", "as", "by", "is", "or", "owen m borseth", "at", "he", "it", "on", "to");
+//$wordArray = file("./data/english.sorted.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$wordArrayCount = count($wordArray);
+echo("Loading $wordArrayCount strings...\n");
+
+$myTernaryTree = new TernaryTree();
+
+$counter = 0;
+foreach($wordArray as $word)
+{
+    $myTernaryTree->insert($word);
+    $counter++;
+
+    if($counter % 1000 == 0)
+	echo(time().": $counter / $wordArrayCount\n");
+}
+
+echo("Done loading.\n\n");
+
+var_dump($myTernaryTree);
+
+$myTernaryTree->traverse();
+
+echo(time()."\n");
+$result = $myTernaryTree->search($searchWord, "i");
+echo(time()."\n");
+echo("iterative result: $result\n");
+echo(time()."\n");
+$result = $myTernaryTree->search($searchWord, "r");
+echo(time()."\n");
+echo("recursive result: $result\n");
+
+?>
